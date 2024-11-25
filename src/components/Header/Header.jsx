2b534fc9db798216f73c4352a41dc498460a3ef1
denis12let <84den84sokol@gmail.com>
2024-11-25 19:27:59 +0300

    header with animations

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import fonbet from '../../assets/images/fonbet.png';
 import header from './Header.module.css';
 import HeaderNavbar from './Navbar/Navbar';
@@ -12,6 +12,12 @@ import { THEME, useTheme } from 'context';
 const Header = () => {
   const { theme, toggleTheme } = useTheme();
   const info = ['asd', 'qwe'];
+  const [isLightTheme, setIsLightTheme] = useState(theme === THEME.LIGHT);
+
+  const handleToggleTheme = () => {
+    setIsLightTheme((prev) => !prev);
+    toggleTheme();
+  };
 
   return (
     <div className={header.header}>
@@ -20,8 +26,11 @@ const Header = () => {
         <HeaderNavbar />
         <TooltipTrigger text="+375 (44) 123 12 23" info={info} />
         <div className={header.settings}>
-          <Button onClick={() => toggleTheme()}>
-            <img src={theme === THEME.LIGHT ? nightTheme : lightTheme} className={header.header__icon} />
+          <Button onClick={handleToggleTheme}>
+            <div className={header.icon__container}>
+              <img src={nightTheme} className={`${header.icon} ${isLightTheme ? header.hidden : ''}`} />
+              <img src={lightTheme} className={`${header.icon} ${isLightTheme ? '' : header.hidden}`} />
+            </div>
           </Button>
           <Avatar />
         </div>
