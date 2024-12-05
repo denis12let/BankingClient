3ae3c1a92bc0009161686f99bc5c09c64d0e84be
denis12let <84den84sokol@gmail.com>
2024-12-06 00:18:09 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import AppRouter from 'components/AppRouter/AppRouter';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <AppRouter />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
